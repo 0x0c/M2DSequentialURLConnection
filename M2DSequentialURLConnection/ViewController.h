60e50fba5e6931ee834d6e90c2314c62b81c9685
Akira Matsuda <akira.matsuda@me.com>
2013-08-04 15:31:14 +0900

    Demo

--- M2DSequentialURLConnection/ViewController.h
+++ M2DSequentialURLConnection/ViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "M2DSequentialURLConnection.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <M2DSequentialURLConnectionDelegate>
 
 @end
--- M2DSequentialURLConnection/ViewController.m
+++ M2DSequentialURLConnection/ViewController.m
@@ -18,12 +18,33 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	M2DSequentialURLConnection *sharedQueue = [M2DSequentialURLConnection sharedQueue];
+	sharedQueue.delegate = self;
+	
+	NSArray *array = @[@"http://www.apple.com", @"http://www.google.com", @"https://github.com"];
+	for (NSString *url in array) {
+		M2DURLConnectionOperation *operation = [[M2DURLConnectionOperation alloc] initWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:url]] completeBlock:^(M2DURLConnection *connection, NSData *data, NSError *error) {
+			if (error) {
+				NSLog(@"%@", error.localizedDescription);
+			}
+			NSLog(@"finished:%@", url);
+		}];
+		[sharedQueue addOperation:operation autoStart:YES];
+	}
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark M2DSequentialURLConnectionDelegate
+
+- (void)sequentiaoURLConnectionDidDequeued:(NSDictionary *)operationInfo
+{
+	M2DURLConnectionOperation *operation = operationInfo[M2DSequentialURLConnectionOperation];
+	NSLog(@"%@(auto start:%d)", operation.connection.identifier, [operationInfo[M2DSequentialURLConnectionAutoStart] boolValue]);
 }
 
 @end
